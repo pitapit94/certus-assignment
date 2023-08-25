334d9ff4173a4f10078e0a2a44f43c2b1d555c69
pitapit94 <141116486+pitapit94@users.noreply.github.com>
2023-08-25 14:48:19 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const initialAccountState = {
   id: 0,
   accountNumber: '',
   transactions : initialTransactionState
-}  
+} //empty state to be used to make sure the undefined does not get sent to the TableDisplay component.
 
 function App() {
 
@@ -35,21 +35,17 @@ function App() {
   const [accumulatedAmount, setAccumulatedAmount] = useState(0); // sets the state of the total amounts selected
   
   const selectAccount = (accountNumber) =>{
-    setSelectedAccount(accountNumber ? accountNumber : {...initialAccountState})
+    setSelectedAccount(accountNumber ? accountNumber : {...initialAccountState}) 
     accountNumber && setSelectedAccount(accountNumber) 
-    console.log("setSelectedAccNumber", accountNumber)
-  }
+  } // checks to see if account number is exsisting and set the state as the selected account (inputed in the searchbox), if not it will set state as an empty object.
   
   const addToAccumulatedAmount = (amount) =>{
     setAccumulatedAmount(accumulatedAmount + amount)
-  }
+  } // accumulates amounts selected from the row and sets it to the state AccumulatedAmount
 
-  const unmatch = ()=>{
+  const unmatch = ()=>{ 
     setAccumulatedAmount(0)
-    console.log(accumulatedAmount)
-  }
-
-    console.log('app.js',accumulatedAmount)
+  } // unmatches whats been accumulated and sets the AccumulatedAmount state as 0
 
   return (
     <div className="App">
